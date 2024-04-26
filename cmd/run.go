66b0aabe98fdea9026a2d33b66b399d6a5cbe3e4
daniel1302 <daniel.1302@gmail.com>
2024-04-26 12:22:41 +0200

    feat: create run and version command for process-watcher

---
+++ cmd/run.go
@@ -0,0 +1,91 @@
+package cmd
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/spf13/cobra"
+	"github.com/vegaprotocol/process-watcher/config"
+	"github.com/vegaprotocol/process-watcher/service"
+	"go.uber.org/zap"
+)
+
+var (
+	configFilePath string
+
+	RunCmd = &cobra.Command{
+		Use:   "run",
+		Short: "A command used to supervise the service",
+		Run: func(cmd *cobra.Command, args []string) {
+			config, err := config.ReadFromFile(configFilePath)
+			if err != nil {
+				panic(err)
+			}
+
+			if err := execute(config); err != nil {
+				panic(err)
+			}
+		},
+	}
+)
+
+func init() {
+	RunCmd.PersistentFlags().StringVarP(
+		&configFilePath,
+		"config-path",
+		"c",
+		"./config.toml",
+		"Path to the config file",
+	)
+}
+
+func execute(config *config.Config) error {
+	programContext, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	logger, err := zap.NewProduction()
+	if err != nil {
+		return fmt.Errorf("failed to create logger: %w", err)
+	}
+	defer func() {
+		if err := logger.Sync(); err != nil {
+			fmt.Printf("failed to sync logger buffer: %s", err.Error())
+		}
+	}()
+
+	failureNotifier := make(chan service.ServiceFailureType)
+
+	logStreamWatcher, err := service.NewLogStreamWatcher(config.Commands.LogStream, failureNotifier)
+	if err != nil {
+		return fmt.Errorf("failed to create new log stream process: %w", err)
+	}
+
+	logsFailureDetector := service.NewKeyWordMatcher(config.LogsWatcher.FailureKeywords)
+	if err := logStreamWatcher.Start(programContext, logger, logsFailureDetector); err != nil {
+		return fmt.Errorf("failed to start log stream process")
+	}
+
+	processWatcher, err := service.NewProcessWatcher(failureNotifier)
+	if err != nil {
+		return fmt.Errorf("failed to create process watcher: %w", err)
+	}
+
+	if err := processWatcher.Start(programContext, logger.Named("process-watcher"), &config.ProcessWatcher); err != nil {
+		return fmt.Errorf("failed to start process watcher: %w", err)
+	}
+
+	serviceManager, err := service.NewServiceManager(
+		failureNotifier,
+		config.Commands.Stop,
+		config.Commands.Start,
+	)
+	if err != nil {
+		return fmt.Errorf("failed to create service manager")
+	}
+	if err := serviceManager.Start(programContext, logger.Named("service-manager")); err != nil {
+		return fmt.Errorf("failed to start service manager : %w", err)
+	}
+
+	<-programContext.Done()
+	return nil
+}
--- main.go
+++ main.go
@@ -1,13 +1,8 @@
 package main
 
 import (
-	"context"
-	"fmt"
-
 	"github.com/spf13/cobra"
-	"github.com/vegaprotocol/process-watcher/config"
-	"github.com/vegaprotocol/process-watcher/service"
-	"go.uber.org/zap"
+	"github.com/vegaprotocol/process-watcher/cmd"
 )
 
 var (
@@ -16,16 +11,6 @@ var (
 	rootCmd = &cobra.Command{
 		Use:   "process-watcher",
 		Short: "A command used to supervise service",
-		Run: func(cmd *cobra.Command, args []string) {
-			config, err := config.ReadFromFile(configFilePath)
-			if err != nil {
-				panic(err)
-			}
-
-			if err := execute(config); err != nil {
-				panic(err)
-			}
-		},
 	}
 )
 
@@ -39,58 +24,10 @@ func init() {
 	)
 }
 
-func execute(config *config.Config) error {
-	programContext, cancel := context.WithCancel(context.Background())
-	defer cancel()
-
-	logger, err := zap.NewProduction()
-	if err != nil {
-		return fmt.Errorf("failed to create logger: %w", err)
-	}
-	defer func() {
-		if err := logger.Sync(); err != nil {
-			fmt.Printf("failed to sync logger buffer: %s", err.Error())
-		}
-	}()
-
-	failureNotifier := make(chan service.ServiceFailureType)
-
-	logStreamWatcher, err := service.NewLogStreamWatcher(config.Commands.LogStream, failureNotifier)
-	if err != nil {
-		return fmt.Errorf("failed to create new log stream process: %w", err)
-	}
-
-	logsFailureDetector := service.NewKeyWordMatcher(config.LogsWatcher.FailureKeywords)
-	if err := logStreamWatcher.Start(programContext, logger, logsFailureDetector); err != nil {
-		return fmt.Errorf("failed to start log stream process")
-	}
-
-	processWatcher, err := service.NewProcessWatcher(failureNotifier)
-	if err != nil {
-		return fmt.Errorf("failed to create process watcher: %w", err)
-	}
-
-	if err := processWatcher.Start(programContext, logger.Named("process-watcher"), &config.ProcessWatcher); err != nil {
-		return fmt.Errorf("failed to start process watcher: %w", err)
-	}
-
-	serviceManager, err := service.NewServiceManager(
-		failureNotifier,
-		config.Commands.Stop,
-		config.Commands.Start,
-	)
-	if err != nil {
-		return fmt.Errorf("failed to create service manager")
-	}
-	if err := serviceManager.Start(programContext, logger.Named("service-manager")); err != nil {
-		return fmt.Errorf("failed to start service manager : %w", err)
-	}
-
-	<-programContext.Done()
-	return nil
-}
-
 func main() {
+	rootCmd.AddCommand(cmd.RunCmd)
+	rootCmd.AddCommand(cmd.VersionCmd)
+
 	if err := rootCmd.Execute(); err != nil {
 		panic(err)
 	}
